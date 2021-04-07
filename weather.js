22e33fd010beb6438b2855a1acea099b275ffc39
Steve Botella <steve.botella@le-campus-numerique.fr>
2021-04-07 16:08:13 +0200

    fix: node_modules missing localy

--- weather.js
+++ weather.js
@@ -3,11 +3,10 @@ let weather = require('weather-js');
 // Options:
 // search:     location name or zipcode
 // degreeType: F or C
+//console.log(JSON.stringify(result, null, 2));
 
 weather.find({search: 'Lyon, France', degreeType: 'F'}, function(err, result) {
     if(err) console.log(err);
-
-    //console.log(JSON.stringify(result, null, 2));
 
     let ville = result[0].location.name;
     let temperature = result[0].current.temperature;
