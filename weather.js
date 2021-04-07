665976a470f18d98594a0a03a71dcd8d6e33cc3c
Steve Botella <steve.botella@le-campus-numerique.fr>
2021-04-07 14:59:21 +0200

    wip: try with Lyon

--- weather.js
+++ weather.js
@@ -1,10 +1,10 @@
-var weather = require('weather-js');
+let weather = require('weather-js');
 
 // Options:
 // search:     location name or zipcode
 // degreeType: F or C
 
-weather.find({search: 'San Francisco, CA', degreeType: 'F'}, function(err, result) {
+weather.find({search: 'Lyon, France', degreeType: 'F'}, function(err, result) {
     if(err) console.log(err);
 
     console.log(JSON.stringify(result, null, 2));
