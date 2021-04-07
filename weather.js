5b1aa98fdfde78b44a41ce14fbc659e4f16ee295
Steve Botella <steve.botella@le-campus-numerique.fr>
2021-04-07 15:27:17 +0200

    feat: exercice etape 4

--- weather.js
+++ weather.js
@@ -7,5 +7,13 @@ let weather = require('weather-js');
 weather.find({search: 'Lyon, France', degreeType: 'F'}, function(err, result) {
     if(err) console.log(err);
 
-    console.log(JSON.stringify(result, null, 2));
+    //console.log(JSON.stringify(result, null, 2));
+
+    let ville = result[0].location.name;
+    let temperature = result[0].current.temperature;
+    let ciel = result[0].current.skytext;
+
+    console.log(ville);
+    console.log(temperature);
+    console.log(ciel);
 });
